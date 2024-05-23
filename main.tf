341676eb7c964a48333f5192c53e82ed6cf60a81
tobblaschke <163132611+tobblaschke@users.noreply.github.com>
2024-05-23 10:28:41 +0200

    tst

--- main.tf
+++ main.tf
@@ -1,4 +1,27 @@
 provider "google" {
   project = "index-icos-sandbox-e9ddv7"
-  region  = "eu-west4"
+  region  = "europe-west4"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "vm-instance"
+  machine_type = "e2-medium"
+  zone         = "europe-west4-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+variable "project_id" {
+  description = "The project ID to deploy resources"
+  type        = string
 }
---
+++ terraform.tfvars.tf
@@ -0,0 +1,1 @@
+project_id = "index-icos-sandbox-e9ddv7"
