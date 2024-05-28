acbc2a2473c4505a6de05872be76209f69cf4da5
tobblaschke <163132611+tobblaschke@users.noreply.github.com>
2024-05-28 13:28:14 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  project = "index-icos-sandbox-e9ddv7"
+  region  = "eu-west4"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "vm-instance"
+  machine_type = "e2-medium"
+  zone         = "europe-west4-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
