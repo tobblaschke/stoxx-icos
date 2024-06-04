9fda53958c86c4cf4642b634a16fec62f871b473
Tobias Blaschke <tblaschke@qontigo.com>
2024-06-04 17:27:40 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "google_compute_instance" "default" {
   }
 
   service_account {
-    email  = "default"
+    email  = "index-icos-sandbox-e9ddv7@appspot.gserviceaccount.com"
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 }
